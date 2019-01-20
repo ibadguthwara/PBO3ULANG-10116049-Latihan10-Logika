7fe64ebfacedc1812a8c50053274ab84e8ce31fb
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-01-20 23:31:02 +0700

    Initial commit

---
+++ src/pbo3ulang/pkg10116049/latihan10/logika/PBO3ULANG10116049Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan10.logika;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA 	     : M. Ibad Guthwara
+ * KELAS	     : PBO 3 ULANG
+ * NIM		     : 10116049
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk logika mengecek nilai apakah true atau false
+ */
+public class PBO3ULANG10116049Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
